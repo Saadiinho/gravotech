93d2f9bedf81d6c9b34ef68ce774986bf1654536
Saad R <saadrafiqul93380@gmail.com>
2026-01-21 21:14:35 +0100

    Update graveuse action

--- gravotech/actions/actions.py
+++ gravotech/actions/actions.py
@@ -13,19 +13,28 @@ class GraveuseAction:
     def __init__(self, streamer: IPStreamer):
         self.streamer = streamer
 
-    def go(self):
-        resp = self.streamer.write("GO\r")
-        while not (
-            resp.startswith("ER") or resp == "GO P" or resp == "GO S" or resp == "GO F"
-        ):
-            resp = self.streamer.read()
-        return resp
+    def go(self) -> str:
+        unlock = self.streamer.lock()
+        try:
+            self.streamer.unsafe_write("GO")
+            resp = self.streamer.unsafe_read()
+            if resp.startswith("ER"):
+                return resp
+            if "GO M" not in resp:
+                raise RuntimeError(f"Expected 'GO M', got '{resp}'")
+            while True:
+                resp = self.streamer.unsafe_read()
+                if resp in ["GO P", "GO S", "GO F"] or resp.startswith("ER"):
+                    return resp
+        finally:
+            unlock()
 
     def ld(self, filename: str, nb_marking: int, mode: LDMode) -> str:
-        return self.streamer.write(f"LD {filename} {nb_marking} {mode}\r")
+        return self.streamer.write(f'LD "{filename}" {nb_marking} {mode.value}\r')
 
     def ls(self, mask: str = None) -> str:
-        return self.streamer.write(f"LS {mask}\r")
+        cmd = f"LS {mask}" if mask else "LS"
+        return self.streamer.write(cmd)
 
     def st(self) -> str:
         return self.streamer.write(f"ST\r")
@@ -34,4 +43,4 @@ class GraveuseAction:
         return self.streamer.write(f"VG {index}\r")
 
     def vs(self, index: int, text: str) -> str:
-        return self.streamer.write(f"VS {index} {text}\r")
+        return self.streamer.write(f'VS {index} "{text}"\r')
